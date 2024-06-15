a733be6e52d78f003c656d3a39901325412e7353
Maria Porcina <maria.porcina.mp@gmail.com>
2024-06-15 16:54:08 -0300

    fix: project info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Managyee Setup'
+copyright = '2024, Maria'
+author = 'Maria'
 
 release = '0.1'
 version = '0.1.0'
